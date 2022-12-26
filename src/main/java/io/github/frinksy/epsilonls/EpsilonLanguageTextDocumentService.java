cc076cf6d310e4fed650793288b6d587bfd58c04
Adam Blanchet <adamblanchet@free.fr>
2022-12-26 19:28:17 +0100

    Refactor diagnostics into separate function.
    Signed-off-by: Adam Blanchet <adamblanchet@free.fr>

--- src/main/java/io/github/frinksy/epsilonls/EpsilonLanguageTextDocumentService.java
+++ src/main/java/io/github/frinksy/epsilonls/EpsilonLanguageTextDocumentService.java
@@ -8,12 +8,18 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 
+// import org.eclipse.emf.common.EMFPlugin;
+// import org.eclipse.emf.ecore.resource.Resource;
+// import org.eclipse.emf.ecore.resource.ResourceSet;
+// import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
 import org.eclipse.epsilon.common.parse.problem.ParseProblem;
+import org.eclipse.epsilon.emc.emf.EmfMetaModel;
+import org.eclipse.epsilon.emc.emf.EmfModel;
 import org.eclipse.epsilon.eol.EolModule;
 import org.eclipse.epsilon.eol.exceptions.EolRuntimeException;
+import org.eclipse.epsilon.eol.exceptions.models.EolModelLoadingException;
 import org.eclipse.lsp4j.DeclarationParams;
 import org.eclipse.lsp4j.Diagnostic;
-import org.eclipse.lsp4j.DiagnosticSeverity;
 import org.eclipse.lsp4j.DidChangeTextDocumentParams;
 import org.eclipse.lsp4j.DidCloseTextDocumentParams;
 import org.eclipse.lsp4j.DidOpenTextDocumentParams;
@@ -43,7 +49,6 @@ public class EpsilonLanguageTextDocumentService implements TextDocumentService {
 
     private Map<TextDocumentItem, List<ParseProblem>> diagnosticsMap;
 
-
     public EpsilonLanguageTextDocumentService(EpsilonLanguageServer languageServer) {
         this.languageServer = languageServer;
 
@@ -51,115 +56,69 @@ public class EpsilonLanguageTextDocumentService implements TextDocumentService {
 
     }
 
-
     @Override
     public void didOpen(DidOpenTextDocumentParams params) {
-        // Let's try to parse the file when it is opened.
-        languageServer.getClient().logMessage(new MessageParams(MessageType.Info, "About to parse a file."));
-        EolModule eolModule = new EolModule();
-        URI textUri = null;
-        try {
-            textUri = new URI(params.getTextDocument().getUri());
-        } catch (URISyntaxException e1) {
-            // TODO Auto-generated catch block
-            e1.printStackTrace();
-        }
-        try {
-            if (eolModule.parse(textUri)) {
-                languageServer.getClient().logMessage(new MessageParams(MessageType.Info, "Successfully parsed file: " + textUri));
-                languageServer.getClient().publishDiagnostics(new PublishDiagnosticsParams(params.getTextDocument().getUri(), new ArrayList<Diagnostic>()));
-            } else {
-                eolModule.execute();
-                List<ParseProblem> parseProblems = eolModule.getParseProblems();
-                this.diagnosticsMap.put(params.getTextDocument(), parseProblems);
-                List<Diagnostic> diagnostics = new ArrayList<>();
-                for (ParseProblem problem: parseProblems) {
-                    languageServer.getClient().logMessage(new MessageParams(MessageType.Error, problem.toString()));
-                    diagnostics.add(
-                        new Diagnostic(
-                            new Range(
-                                new Position(problem.getLine(), problem.getColumn()),
-                                new Position(problem.getLine(), problem.getColumn())),
-                                problem.getReason()
-                            )
-                    );
-                }
 
+        List<Diagnostic> diagnostics = generateDiagnostics(params.getTextDocument().getText(),
+                URI.create(params.getTextDocument().getUri()));
 
-                this.languageServer.getClient().publishDiagnostics(new PublishDiagnosticsParams(params.getTextDocument().getUri(), diagnostics));
+        this.languageServer.getClient().publishDiagnostics(
+                new PublishDiagnosticsParams(params.getTextDocument().getUri(), diagnostics));
 
-                // throw new Exception();
-            }
-
-        } catch (EolRuntimeException eolRuntimeException) {
-
-        }
-        catch (Exception e) {
-            languageServer.getClient().logMessage(new MessageParams(MessageType.Error, "Could not parse file: " + textUri));
-        }
-
-        
     }
 
     @Override
     public void didChange(DidChangeTextDocumentParams params) {
-        
+        languageServer.getClient()
+                .logMessage(new MessageParams(MessageType.Info, "File changed: " + params.getTextDocument().getUri()));
         // Update server's text document version
         List<TextDocumentContentChangeEvent> changes = params.getContentChanges();
         String docUri = params.getTextDocument().getUri();
-        
-        for (TextDocumentContentChangeEvent change: changes) {
-            List<Diagnostic> diagnostics = generateDiagnostics(change.getText(),null);
+
+        for (TextDocumentContentChangeEvent change : changes) {
+            List<Diagnostic> diagnostics = generateDiagnostics(change.getText(), null);
             languageServer.getClient().publishDiagnostics(new PublishDiagnosticsParams(docUri, diagnostics));
         }
-        
-        
 
-
-        
     }
 
     @Override
     public void didClose(DidCloseTextDocumentParams params) {
         // TODO Auto-generated method stub
-        
+
     }
 
     @Override
     public void didSave(DidSaveTextDocumentParams params) {
         // TODO Auto-generated method stub
-        
+
     }
-    
 
     @Override
     public CompletableFuture<Hover> hover(HoverParams params) {
         languageServer.getClient().logMessage(new MessageParams(MessageType.Info, "Got a new hover request."));
-        
+
         Hover hover = new Hover();
         MarkupContent contents = new MarkupContent(MarkupKind.PLAINTEXT, "This is a test hover text");
         hover.setContents(contents);
-    
-
 
         return CompletableFuture.supplyAsync(() -> hover);
     }
 
     @Override
-    public CompletableFuture<Either<List<? extends Location>, List<? extends LocationLink>>> declaration(DeclarationParams params) {
-     
+    public CompletableFuture<Either<List<? extends Location>, List<? extends LocationLink>>> declaration(
+            DeclarationParams params) {
+
         languageServer.getClient().logMessage(new MessageParams(MessageType.Info, "Got a goto declaration request."));
 
         Location new_location = new Location();
         new_location.setRange(
-            new Range(new Position(params.getPosition().getLine() + 1, params.getPosition().getCharacter()),
-            new Position(params.getPosition().getLine() + 1, params.getPosition().getCharacter()))
-        );
+                new Range(new Position(params.getPosition().getLine() + 1, params.getPosition().getCharacter()),
+                        new Position(params.getPosition().getLine() + 1, params.getPosition().getCharacter())));
         new_location.setUri(params.getTextDocument().getUri());
-    
+
         List<Location> res = new ArrayList<Location>();
         res.add(new_location);
-
 
         return CompletableFuture.supplyAsync(() -> Either.forLeft(res));
 
@@ -167,45 +126,16 @@ public class EpsilonLanguageTextDocumentService implements TextDocumentService {
 
     @Override
     public CompletableFuture<DocumentDiagnosticReport> diagnostic(DocumentDiagnosticParams params) {
-        this.languageServer.getClient().logMessage(new MessageParams(MessageType.Info, "Going to generate diagnostics."));
-        URI documentUri = null;
+        this.languageServer.getClient()
+                .logMessage(new MessageParams(MessageType.Info, "Going to generate diagnostics."));
 
         List<Diagnostic> diagnostics = new ArrayList<>();
-        
-        try {
-            documentUri = new URI(params.getTextDocument().getUri());
-        } catch (URISyntaxException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
 
-
-        if (documentUri!=null) {
-            List<ParseProblem> parseProblems = diagnosticsMap.get(documentUri);
-
-
-            for (ParseProblem problem: parseProblems) {
-                this.languageServer.getClient().logMessage(new MessageParams(MessageType.Info, "Parse problem added to the diagnostics"));
-                Diagnostic diagnostic = new Diagnostic(
-                    new Range(
-                        new Position(problem.getLine(), problem.getColumn()),
-                        new Position(problem.getLine(), problem.getColumn())
-                    ),
-                    problem.getReason()
-                );
-
-                DiagnosticSeverity severity = DiagnosticSeverity.forValue(problem.getSeverity());
-                diagnostic.setSeverity(severity);
-                diagnostics.add(diagnostic);
-            }
-
-        }
-
-        DocumentDiagnosticReport report = new DocumentDiagnosticReport(new RelatedFullDocumentDiagnosticReport(diagnostics));
+        DocumentDiagnosticReport report = new DocumentDiagnosticReport(
+                new RelatedFullDocumentDiagnosticReport(diagnostics));
 
         return CompletableFuture.supplyAsync(() -> report);
-        
-        
+
     }
 
     private List<Diagnostic> generateDiagnostics(String text, URI textUri) {
@@ -214,40 +144,76 @@ public class EpsilonLanguageTextDocumentService implements TextDocumentService {
         List<Diagnostic> diagnostics = new ArrayList<>();
         try {
             if (eolModule.parse(text, null)) {
-                languageServer.getClient().logMessage(new MessageParams(MessageType.Info, "Successfully parsed file: " + textUri));
-                return new ArrayList<Diagnostic>();
+                languageServer.getClient()
+                        .logMessage(new MessageParams(MessageType.Info, "Successfully parsed file: " + textUri));
+
             } else {
-                eolModule.execute();
+
                 List<ParseProblem> parseProblems = eolModule.getParseProblems();
 
-                
-                for (ParseProblem problem: parseProblems) {
+                for (ParseProblem problem : parseProblems) {
                     languageServer.getClient().logMessage(new MessageParams(MessageType.Error, problem.toString()));
                     diagnostics.add(
-                        new Diagnostic(
-                            new Range(
-                                new Position(problem.getLine(), problem.getColumn()),
-                                new Position(problem.getLine(), problem.getColumn())),
-                                problem.getReason()
-                            )
-                    );
+                            new Diagnostic(
+                                    new Range(
+                                            new Position(problem.getLine(), problem.getColumn()),
+                                            new Position(problem.getLine(), problem.getColumn())),
+                                    problem.getReason()));
                 }
 
-
-                
-
             }
+        } catch (EolModelLoadingException eolParseException) {
+            languageServer.getClient().logMessage(
+                    new MessageParams(MessageType.Error, "Error parsing model: " + eolParseException.getMessage()));
 
-        } catch (EolRuntimeException eolRuntimeException) {
-
+        } catch (Exception e) {
+            languageServer.getClient()
+                    .logMessage(new MessageParams(MessageType.Error, "Could not parse file: " + textUri));
         }
-        catch (Exception e) {
-            languageServer.getClient().logMessage(new MessageParams(MessageType.Error, "Could not parse file: " + textUri));
-        }
-
 
         return diagnostics;
     }
 
+    /**
+     * Run a module with a metamodel, and get the resulting diagnostics.
+     * 
+     * @param textUri
+     * @param eolModule
+     */
+    public void ExecuteModule(String textUri, EolModule eolModule) {
+        languageServer.getClient()
+                .logMessage(new MessageParams(MessageType.Info, "About to execute file: " + textUri));
+        try {
+            // Get the model
+            EmfModel mymodel = new EmfModel();
+            mymodel.setMetamodelFile("/path/to/metamodel.emf");
+
+            EmfMetaModel mymeta = new EmfMetaModel(
+                    "file:///path/to/metamodel.emf");
+            mymeta.loadModel();
+            mymeta.close();
+
+            // mymodel.setModelFile("/path/to/model.flexmi");
+
+            // ResourceSet resourceSet = new ResourceSetImpl();
+            // resourceSet.getResourceFactoryRegistry().
+            // getExtensionToFactoryMap().put("flexmi",
+            // new FlexmiResourceFactory());
+            // Resource resource =
+            // resourceSet.createResource(URI.createFileURI("/../acme.flexmi"));
+            // resource.load(null);
+
+            mymodel.setName("M");
+            mymodel.setReadOnLoad(true);
+            mymodel.setStoredOnDisposal(false);
+            mymodel.load();
+            eolModule.getContext().getModelRepository().addModel(mymodel);
+
+            eolModule.execute();
+        } catch (EolRuntimeException eolRuntimeException) {
+            languageServer.getClient().logMessage(
+                    new MessageParams(MessageType.Error, "Error running module: " + eolRuntimeException.getMessage()));
+        }
+    }
 
 }
