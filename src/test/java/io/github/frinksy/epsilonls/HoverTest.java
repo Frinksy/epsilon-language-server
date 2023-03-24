1dcc9707f6497d5d9a27489290d14c11b7dbc1ba
Adam Blanchet <adamblanchet@free.fr>
2023-03-24 11:49:23 +0100

    HoverTest: add second EOL document.
    Signed-off-by: Adam Blanchet <adamblanchet@free.fr>

--- src/test/java/io/github/frinksy/epsilonls/HoverTest.java
+++ src/test/java/io/github/frinksy/epsilonls/HoverTest.java
@@ -20,8 +20,7 @@ import org.junit.jupiter.api.Test;
 
 public class HoverTest {
 
-    EolDocument document;
-    Path testFilePath = Paths.get(".", "src", "test", "resources", "pens.eol");
+    EolDocument document, document2;
 
     @BeforeEach
     void registerMetamodels() throws Exception {
@@ -34,19 +33,26 @@ public class HoverTest {
 
     @BeforeEach
     void setFileContents() throws IOException {
+        Path resourcesPath = Paths.get(".", "src", "test", "resources");
+        Path testFilePath = resourcesPath.resolve("pens.eol");
+        Path testFilePath2 = resourcesPath.resolve("random_program.eol");
 
         document = new EolDocument(new MockedLanguageServer(), testFilePath.toUri().toString());
         document.setContents(Files.readString(testFilePath));
+
+        document2 = new EolDocument(new MockedLanguageServer(), testFilePath2.toUri().toString());
+        document2.setContents(Files.readString(testFilePath2));
+
     }
 
-    void testHoverText(String expectedHoverText, int colStart, int colEnd, int line, String message) {
+    void testHoverText(EolDocument doc, String expectedHoverText, int colStart, int colEnd, int line, String message) {
 
         for (int col = colStart; col <= colEnd; col++) {
             Position hoveredPosition = new Position(line, col);
 
-            MarkupContent actualMarkupContent = document.getHoverContents(
+            MarkupContent actualMarkupContent = doc.getHoverContents(
                     new HoverParams(
-                            new TextDocumentIdentifier(testFilePath.toUri().toString()), hoveredPosition));
+                            new TextDocumentIdentifier(doc.getFilename()), hoveredPosition));
 
             assertEquals(expectedHoverText, actualMarkupContent.getValue(), message);
 
@@ -61,7 +67,7 @@ public class HoverTest {
         // call of getFullName on p at
         // line 10 (-1)
         // cols 6 - 16
-        testHoverText(expectedHoverText, 6, 16, 9, null);
+        testHoverText(document, expectedHoverText, 6, 16, 9, null);
     }
 
     @Test
@@ -72,7 +78,7 @@ public class HoverTest {
         // operation definition at
         // line 13 (-1)
         // col 17 - 27
-        testHoverText(expectedHoverText, 17, 27, 12, null);
+        testHoverText(document, expectedHoverText, 17, 27, 12, null);
     }
 
 }
